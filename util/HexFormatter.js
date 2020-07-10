4cdb690f543ecfad04c64f45d521a8c64cb9815a
xubinnan <xubinnan@bytedance.com>
2020-07-10 16:42:35 +0800

    fix: v1.0.1 replace path

--- util/HexFormatter.js
+++ util/HexFormatter.js
@@ -3,7 +3,7 @@ const replace = require('replace');
 replace({
   regex: /#([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])(['"])/g,
   replacement: '#$1$1$2$2$3$3$4',
-  paths: ['node_modules/@antv/f2'],
+  paths: ['../@antv/f2'],
   recursive: true,
   silent: true,
 });
