16eedda9c35dc2b97ef7155ed0b92449b24a3538
xubinnan <xubinnan@bytedance.com>
2021-05-12 17:47:34 +0800

    fix: fontSize 小数正则

--- util/getCanvasContext.js
+++ util/getCanvasContext.js
@@ -15,8 +15,8 @@ const MAP = {
   textBaseline: "setTextBaseline",
 };
 
-// 提取可配置的 fontSize，支持整数 / 浮点数
-const fontSizeReg = /([1-9]*\d?(\.\d{1,2})?)px/;
+// 提取可配置的 fontSize，支持整数 / 浮点数（fontSize 不应有负数）
+const fontSizeReg = /(\d*(\.\d*)?)px/;
 
 export default (ctx) => {
   Object.keys(MAP).forEach((key) => {
